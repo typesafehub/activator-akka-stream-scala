93f85e32464d90404acc827dc1191d8eb4c10a0f
Tim Harper <timcharper@gmail.com>
2014-12-24 11:57:06 -0700

    Fixes GroupLogFile example early-shutdown
    Yield a future for each group and wait for them to complete prior to shutting down the ActorSystem

--- src/main/scala/sample/stream/GroupLogFile.scala
+++ src/main/scala/sample/stream/GroupLogFile.scala
@@ -1,10 +1,9 @@
 package sample.stream
 
 import akka.actor.ActorSystem
+import akka.stream.scaladsl.{ Sink, Source }
+import akka.stream.{ FlowMaterializer, OverflowStrategy }
 import java.io.{ FileOutputStream, PrintWriter }
-import akka.stream.FlowMaterializer
-import akka.stream.scaladsl.Source
-
 import scala.util.Try
 
 object GroupLogFile {
@@ -18,6 +17,7 @@ object GroupLogFile {
     implicit val materializer = FlowMaterializer()
 
     val LoglevelPattern = """.*\[(DEBUG|INFO|WARN|ERROR)\].*""".r
+    val maximumLogLevelCount = 5
 
     // read lines from a log file
     val logFile = io.Source.fromFile("src/main/resources/logfile.txt", "utf-8")
@@ -29,16 +29,20 @@ object GroupLogFile {
         case other                  => "OTHER"
       }.
       // write lines of each group to a separate file
-      foreach {
+      map {
         case (level, groupFlow) =>
           val output = new PrintWriter(new FileOutputStream(s"target/log-$level.txt"), true)
           // close resource when the group stream is completed
           // foreach returns a future that we can key the close() off of
-          groupFlow.foreach(line => output.println(line)).onComplete(_ => Try(output.close()))
+          groupFlow.
+            foreach(line => output.println(line)).
+            andThen { case t => output.close(); t.get }
       }.
-      onComplete { _ =>
+      buffer(maximumLogLevelCount, OverflowStrategy.error).
+      mapAsync(identity).
+      runWith(Sink.onComplete { _ =>
         Try(logFile.close())
         system.shutdown()
-      }
+      })
   }
 }
